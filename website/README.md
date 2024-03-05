0728d76280c7feb32c578c1c0ec37dbd6e8a06c9
Ugo Muhieddine <ugo.muhieddine@student-cs.fr>
2024-03-05 22:27:22 +0100

    first push

---
+++ website/README.md
@@ -0,0 +1,8 @@
+# for locally:
+
+1. run in terminal:
+cd /Users/muhieddineugo/MH2D_projets/expo_photo/website
+python -m http.server
+
+2. then on chrome:
+http://localhost:8000/
---
+++ website/main.js
@@ -0,0 +1,19 @@
+const gallery = document.getElementById('gallery');
+const audioPlayer = document.getElementById('audioPlayer');
+
+fetch('data.json')
+    .then(response => response.json())
+    .then(data => {
+        data.items.forEach((item, index) => {
+            const photoElement = document.createElement('div');
+            photoElement.className = 'photo';
+            photoElement.innerHTML = `<img src="portraits/${item.photo}" alt="Photo ${index + 1}" onclick="playSong('/musique_portrait/${item.song}')">`;
+            gallery.appendChild(photoElement);
+        });
+    })
+    .catch(error => console.error('Error loading data:', error));
+
+function playSong(song) {
+    audioPlayer.src = song;
+    audioPlayer.play();
+}
