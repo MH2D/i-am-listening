5d31c0d860992b2393ec29f7291389a17efc2eaa
Ugo Muhieddine <ugo.muhieddine@student-cs.fr>
2024-03-08 11:58:09 +0100

    feat: make the cotour red when listening

--- main.js
+++ main.js
@@ -4,10 +4,33 @@ const audioPlayer = document.getElementById('audioPlayer');
 fetch('data.json')
     .then(response => response.json())
     .then(data => {
+        let lastClickedPhoto; // Variable to store the last clicked photo
+
         data.items.forEach((item, index) => {
             const photoElement = document.createElement('div');
             photoElement.className = 'photo';
-            photoElement.innerHTML = `<img src="portraits/${item.photo}" alt="Photo ${index + 1}" onclick="playSong('${item.song}')">`;
+
+            const imgElement = document.createElement('img');
+            imgElement.src = `portraits/${item.photo}`;
+            imgElement.alt = `Photo ${index + 1}`;
+
+            imgElement.addEventListener('click', () => {
+                // Check if there was a previously clicked photo, and remove its style
+                if (lastClickedPhoto) {
+                    lastClickedPhoto.classList.remove('photo-clicked');
+                }
+
+                // Update the style of the clicked photo
+                photoElement.classList.add('photo-clicked');
+
+                // Update the last clicked photo
+                lastClickedPhoto = photoElement;
+
+                // Call the playSong function
+                playSong(item.song);
+            });
+
+            photoElement.appendChild(imgElement);
             gallery.appendChild(photoElement);
         });
     })
@@ -17,3 +40,4 @@ function playSong(song) {
     audioPlayer.src = song;
     audioPlayer.play();
 }
+
